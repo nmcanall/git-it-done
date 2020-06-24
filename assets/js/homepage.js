4bede1c11cd17fd015c52d0f14d12f0ed9d50f39
nmcanall <nmcanall@citadel.edu>
2020-06-23 20:23:12 -0700

    fetch GitHub repo API by a username submitted by the user

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,8 @@
+// Global variables
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+// API function to fetch data for a particular user from GitHub
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
@@ -8,4 +13,19 @@ var getUserRepos = function(user) {
     });
 }
 
-getUserRepos("nmcanall");
+// Handler for form submit
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }
+    else {
+        alert("You must submit a username.");
+    }
+}
+
+// Listener for username submit button
+userFormEl.addEventListener("submit", formSubmitHandler);
